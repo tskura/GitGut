244a06b3224f88cf394dc8c5c2f2a270c74ab110
EP <EP@DESKTOP-JR8MDT4>
2018-01-29 21:51:37 +0100

    New classes

---
+++ GitGut/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitGut
+{
+    class Class1
+    {
+        public string DoSomething()
+        {
+            var a = "dasda";
+            return a;
+        }
+
+        public string DoSomethingDifferent()
+        {
+            var a = "dddd" +
+                    "retu";
+            return a;
+        }
+    }
+}
